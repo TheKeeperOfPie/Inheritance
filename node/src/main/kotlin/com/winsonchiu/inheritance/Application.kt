a9f75ded07a38563dd2f7706f9ac69a1b1144c6b
TheKeeperOfPie <igpx346@gmail.com>
2017-06-04 17:50:04 -0400

    Make into a gradle project and add Kotlin

---
+++ node/src/main/kotlin/com/winsonchiu/inheritance/Application.kt
@@ -0,0 +1,13 @@
+package com.winsonchiu.inheritance
+
+/**
+ * Created by TheKeeperOfPie on 6/4/2017.
+ */
+
+object Application {
+
+    @JvmStatic
+    fun main(args : Array<String>) {
+        println("Hello, world!")
+    }
+}
